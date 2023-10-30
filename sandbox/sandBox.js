6e8987c05d7cddb751c2f697ac6ac868ffc4368c
Petr Sidorov <piterpatrikk@gmail.com>
2023-10-30 17:13:46 +0400

    linear search, binary search, Two Crystall Balls problem start

---
+++ sandbox/sandBox.js
@@ -0,0 +1,6 @@
+function two_crystal_balls(breaks) {
+    let lo = 0;
+    let hi = Math.sqrt(breaks.length);
+}
+
+two_crystal_balls([false, false, false]);
---
+++ src/day1/TwoCrystalBalls.ts
@@ -0,0 +1,4 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    let lo = 0;
+    let hi = Math.sqrt(breaks.length);
+}
